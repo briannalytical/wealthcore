db0a3dfa4a1ee1e6ea80fd73517da76e6065b504
Brianna McNeil <briannamcneil.dev@gmail.com>
2025-10-28 19:43:11 -0400

    initial commit | project created

---
+++ src/main/java/com/briannalytical/wealthcore/WealthcoreApplication.java
@@ -0,0 +1,13 @@
+package com.briannalytical.wealthcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthcoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/briannalytical/wealthcore/WealthcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briannalytical.wealthcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
