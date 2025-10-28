4b2ddd9598d3335280fb503a8bb3638ed152d891
Brianna McNeil <briannamcneil.dev@gmail.com>
2025-10-28 19:52:27 -0400

    user model

---
+++ src/main/java/com/briannalytical/wealthcore/Model/User.java
@@ -0,0 +1,55 @@
+package com.briannalytical.wealthcore.Model;
+
+public class User {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String role;
+
+    private boolean isEnabled;
+
+
+    public User() {}
+
+    public User(Long id, String username, String password, String email, String role, boolean isEnabled) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.isEnabled = isEnabled;
+    }
+
+    // getters
+    public Long getId() {return id;}
+
+    public String getUsername() {return username;}
+
+    public String getPassword() {return password;}
+
+    public String getEmail() {return email;}
+
+    public String getRole() {return role;}
+
+    public boolean isEnabled() {return isEnabled;}
+
+
+    // setters
+    public void setId(Long id) {this.id = id;}
+
+    public void setUsername(String username) {this.username = username;}
+
+    public void setPassword(String password) {this.password = password;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public void setRole(String role) {this.role = role;}
+
+    public void setEnabled(boolean enabled) {isEnabled = enabled;}
+}
