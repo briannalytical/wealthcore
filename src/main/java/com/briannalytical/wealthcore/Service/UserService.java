1e6c2b621f8e8526e1f7c83f85a9f1f327664a49
Brianna McNeil <briannamcneil.dev@gmail.com>
2025-10-28 20:09:53 -0400

    user service class

--- src/main/java/com/briannalytical/wealthcore/Service/UserService.java
+++ src/main/java/com/briannalytical/wealthcore/Service/UserService.java
@@ -1,4 +1,47 @@
 package com.briannalytical.wealthcore.Service;
 
+import com.briannalytical.wealthcore.Model.Entity.User;
+import com.briannalytical.wealthcore.Model.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    public User registerUser(String username, String password, String email) {
+        if (userRepository.findByUsername(username).isPresent()) {
+            throw new RuntimeException("Username already exists");
+        }
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setEmail(email);
+        user.setRole("USER");
+        user.setEnabled(true);
+
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+    
+    public boolean existsByUsername(String username) {
+        return userRepository.findByUsername(username).isPresent();
+    }
 }
