ab2e727a5a1d623f0b5d9c92d70e652f14d8b6e6
Brianna McNeil <briannamcneil.dev@gmail.com>
2025-10-29 17:14:33 -0400

    create token method in JwtUtil

--- src/main/java/com/briannalytical/wealthcore/Security/JwtUtil.java
+++ src/main/java/com/briannalytical/wealthcore/Security/JwtUtil.java
@@ -1,19 +1,32 @@
 package com.briannalytical.wealthcore.Security;
 
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 
 
 @Component
-    public class JwtUtil {
+public class JwtUtil {
 
-        // secret key for jwt tokens * store in environment variables *
-        private final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    // secret key for jwt tokens * store in environment variables *
+    private final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
-        // set token validity to 10 hours
-        private static final long JWT_TOKEN_VALIDITY = 10 * 60 * 60 * 1000;
+    // set token validity to 10 hours
+    private static final long JWT_TOKEN_VALIDITY = 10 * 60 * 60 * 1000;
 
+    // create JWT token
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY))
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
 }
