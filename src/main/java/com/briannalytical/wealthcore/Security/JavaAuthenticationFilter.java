84d916c8bc5942db574723a9b053efa210b72be8
Brianna McNeil <briannamcneil.dev@gmail.com>
2025-10-30 11:53:04 -0400

    created JavaAuthenticationFilter class

---
+++ src/main/java/com/briannalytical/wealthcore/Security/JavaAuthenticationFilter.java
@@ -0,0 +1,17 @@
+package com.briannalytical.wealthcore.Security;
+
+
+import com.briannalytical.wealthcore.Service.UserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+}
