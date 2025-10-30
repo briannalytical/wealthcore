4fb0ba307a8748ca5220396c43a42455f33bdba0
Brianna McNeil <briannamcneil.dev@gmail.com>
2025-10-30 10:55:08 -0400

    set environment variables for key and expiration

--- src/main/java/com/briannalytical/wealthcore/Security/JwtUtil.java
+++ src/main/java/com/briannalytical/wealthcore/Security/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -15,6 +16,13 @@ import java.util.function.Function;
 
 @Component
 public class JwtUtil {
+
+    // token secret and expiration TODO: app to application.properties
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long jwtExpiration;
 
     // secret key for jwt tokens * store in environment variables *
     private final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
