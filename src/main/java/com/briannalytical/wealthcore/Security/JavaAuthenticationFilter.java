a6adbf56e6b94717a783ac614fa24258789ce2e8
Brianna McNeil <briannamcneil.dev@gmail.com>
2025-10-30 13:37:13 -0400

    JavaAuthentication class additions

--- src/main/java/com/briannalytical/wealthcore/Security/JavaAuthenticationFilter.java
+++ src/main/java/com/briannalytical/wealthcore/Security/JavaAuthenticationFilter.java
@@ -1,10 +1,15 @@
 package com.briannalytical.wealthcore.Security;
 
-
 import com.briannalytical.wealthcore.Service.UserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -14,4 +19,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        // Get Authorization header
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+    }
 }
