50991e271a5160c1bc4c244992cd95b40f0fd70d
Brianna McNeil <briannamcneil.dev@gmail.com>
2025-10-30 13:52:31 -0400

    JavaAuthenticationFilter refactored

--- src/main/java/com/briannalytical/wealthcore/Security/JavaAuthenticationFilter.java
+++ src/main/java/com/briannalytical/wealthcore/Security/JavaAuthenticationFilter.java
@@ -24,11 +24,4 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        // Get Authorization header
-        final String authorizationHeader = request.getHeader("Authorization");
-
-        String username = null;
-        String jwt = null;
-    }
-
 }
