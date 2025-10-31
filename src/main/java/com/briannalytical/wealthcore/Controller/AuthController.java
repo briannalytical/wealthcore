ef5d86f2871cdbe332d780a2ddf1fdc8fc313d66
Brianna McNeil <briannamcneil.dev@gmail.com>
2025-10-30 21:02:45 -0400

    created and begun work on auth controller

--- src/main/java/com/briannalytical/wealthcore/Controller/AuthController.java
+++ src/main/java/com/briannalytical/wealthcore/Controller/AuthController.java
@@ -1,4 +1,28 @@
 package com.briannalytical.wealthcore.Controller;
 
+import com.briannalytical.wealthcore.Security.JwtUtil;
+import com.briannalytical.wealthcore.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = "*") // Allow frontend to access - adjust in production
 public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 }
